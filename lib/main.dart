bea1f4536c47466860abe227fb65f53ec83e820e
BarnabeLB <75079711+BarnabeLB@users.noreply.github.com>
2021-07-05 18:10:56 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/screens/tabs_screen.dart';
 
-import './categories_screen.dart';
+
+import './screens/tabs_screen.dart';
+import './screens/meal_detail_screen.dart';
+import './screens/categories_screen.dart';
+import './screens/category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,9 +15,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Repas Délicieux',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
       ),
-      home: CategeoriesScreen(),
+      // home: CategeoriesScreen(),
+      initialRoute: '/',
+      /*the default route is '/' */
+      routes: {
+        '/': (ctx) => TabsScreen(),  
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        // mise en lien avec static const routeName = 'category-meals', dans category_meals_screen
+        // '/category-meal' : (ctx) => CategoriesMealsScreen(); ça c'est l'ancienne version, il est préférable de linker avec la prop static pour les gros applications
+        MealDetailScreen.routeName: (ctx)=> MealDetailScreen(),     
+      },
+      // onGenerateRoute: (settings){
+      //   print(settings.arguments);
+      //   return MaterialPageRoute(builder: (ctx)=> CategeoriesScreen());
+      // },
+      onUnknownRoute: (settings){
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
     );
   }
 }
